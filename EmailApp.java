8463d8090f448f0a1e488c5317167ada72c37ac6
kalpadas599 <kalpadas599@gmail.com>
2025-11-13 11:48:20 +0530

    first commit

---
+++ EmailApp.java
@@ -0,0 +1,8 @@
+public class EmailApp {
+    public static void main(String[] args) {
+        // Your code here
+        Email em1 = new Email("Kalpa" , "Das");
+
+        System.out.println(em1.showInfo());
+    }
+}
